40c101b729b181369e47bf8977b25c1324ac2dee
Meredith Espinosa <goclonefilms@gmail.com>
2019-03-03 17:01:16 -0800

    more conveyor chanes

--- src/main/java/io/github/cottonmc/um/block/entity/ConveyorEntity.java
+++ src/main/java/io/github/cottonmc/um/block/entity/ConveyorEntity.java
@@ -7,8 +7,10 @@ import io.github.cottonmc.um.component.SimpleItemComponent;
 import io.github.cottonmc.um.component.wrapper.SidedItemView;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
 import net.minecraft.block.InventoryProvider;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SidedInventory;
@@ -56,7 +58,9 @@ public class ConveyorEntity extends BlockEntity {
 	public void pulse() {
 		if (world==null || items.isEmpty()) return;
 		BlockPos dropoff = findDropoff();
+		System.out.println(items.get(0).getItem());
 		 if (world.getBlockState(pos).get(ConveyorBlock.STATUS) == MachineStatus.INACTIVE) {
+		 	System.out.println("Scheduling transfer!");
 			 double diff = pos.distanceTo(dropoff);
 			 world.setBlockState(pos, world.getBlockState(pos).with(ConveyorBlock.STATUS, MachineStatus.ACTIVE));
 			 world.getBlockTickScheduler().schedule(pos, UMBlocks.CONVEYOR, (int)diff);
@@ -65,8 +69,8 @@ public class ConveyorEntity extends BlockEntity {
 		 	Block block = state.getBlock();
 		 	Direction insertSide = world.getBlockState(pos).get(ConveyorBlock.FACING).getOpposite();
 		 	if (block instanceof InventoryProvider) {
+		 		System.out.println("Inserting into provider!");
 		 		SidedInventory inv = ((InventoryProvider) block).getInventory(state, world, dropoff);
-		 		System.out.println(inv);
 		 		if (inv != null) {
 					int insertSlot = getAvailableSlot(inv, (inv).getInvAvailableSlots(insertSide));
 					if (insertSlot >= 0) {
@@ -75,12 +79,28 @@ public class ConveyorEntity extends BlockEntity {
 						items.removeInvStack(0);
 					}
 				}
-		 	} else if (state.isAir()) {
+		 	} else if (world.getBlockEntity(dropoff) instanceof Inventory) {
+		 		System.out.println("Inserting into inventory!");
+		 		Inventory inv = (Inventory)world.getBlockEntity(dropoff);
+				if (inv instanceof ChestBlockEntity && block instanceof ChestBlock) {
+					inv = ChestBlock.method_17458(state, world, dropoff, true);
+				}
+				if (inv != null) {
+					int insertSlot = getAvailableSlot(inv);
+					if (insertSlot >= 0) {
+						inv.setInvStack(insertSlot, items.getInvStack(0));
+						inv.markDirty();
+						items.removeInvStack(0);
+					}
+				} else System.out.println("Null inventory!");
+			} else if (state.isAir()) {
+		 		System.out.println("Dropping item!");
 				ItemEntity item = new ItemEntity(world, dropoff.getX(), dropoff.getY(), dropoff.getZ(), items.getInvStack(0));
 				items.removeInvStack(0);
 				world.spawnEntity(item);
 			}
 		 	if (!needsPulse()) world.setBlockState(pos, world.getBlockState(pos).with(ConveyorBlock.STATUS, MachineStatus.INACTIVE));
+		 	else world.getBlockTickScheduler().schedule(pos, UMBlocks.CONVEYOR, 1);
 		 }
 
 	}
@@ -109,6 +129,13 @@ public class ConveyorEntity extends BlockEntity {
 
 	public int getAvailableSlot(Inventory inv, int[] slots) {
 		for (int i : slots) {
+			if (inv.getInvStack(i).isEmpty()) return i;
+		}
+		return -1;
+	}
+
+	public int getAvailableSlot(Inventory inv) {
+		for (int i = 0; i < inv.getInvSize(); i++) {
 			if (inv.getInvStack(i).isEmpty()) return i;
 		}
 		return -1;
